6a54f8702168e8593e1b07d4ae03649364a2ae6c
Vitalik Yashchuk <yashchuk_ak21@nuwm.edu.ua>
2023-12-02 14:19:34 +0200

    add

---
+++ Clab3/Clab3.cpp
@@ -0,0 +1,242 @@
+﻿#include <iostream>
+#include <ctime>
+#include <iomanip>
+using std::cout;
+using std::endl;
+using std::cin;
+
+int main()
+{
+    /*const int SIZE = 10;
+    int arr[SIZE];
+	srand(time(0));
+	for (size_t i = 0; i < SIZE; i++)
+	{
+		arr[i] = 1 + rand() % 20;
+	}
+
+	for (size_t i = 0; i < SIZE; i++)
+	{
+		cout << arr[i] << "\t";
+	}
+	cout << endl;
+
+	int min = arr[0];
+	int max = arr[0];
+	int pos_max = 0, pos_min = 0;
+	for (size_t i = 1; i < SIZE; i++)
+	{
+		if (min > arr[i]) {
+			min = arr[i];
+			pos_max = i;
+		}
+		if (max < arr[i]) {
+			max = arr[i];
+			pos_min = i;
+		}
+	}
+	cout << "Min element :: " << min << endl;*/
+
+	//const int SIZE = 10;
+	//int arr[SIZE];
+	//srand(time(0));
+	//for (int i = 0; i < SIZE;)
+	//{
+	//	bool flag = true;
+	//	int number = rand() % 10 + 1;
+	//	for (int j = 0; j < i; j++)
+	//	{
+	//		if (number == arr[j])
+	//		{
+	//			flag = false;
+	//			break;
+	//		}
+	//	}
+	//	if (flag)
+	//	{
+	//		arr[i] = number;
+	//		i++;
+	//	}
+	//}
+	//for (size_t i = 0; i < SIZE; i++)
+	//{
+	//	cout << arr[i] << "\t";
+	//}
+
+	/////////////////////////////Task-1//////////////////////////////////////////
+	
+	//task_1
+	 
+	//int arr[5];
+	//int number, sum=0;
+	//for (int i = 0; i < 6; i++)
+	//{
+	//	cout << "Enter your profit for "<<i+1<<" mounth ::";
+	//	cin >> number;
+	//	arr[i] = number;
+	//}
+	//for (int i = 0; i < 6; i++)
+	//{
+	//	sum += arr[i];
+	//}
+	//cout << "Rezault :: " << sum;
+
+	//task_2
+
+	//const int SIZE = 6;
+	//int arr[SIZE];
+	//srand(time(0));
+	//for (int i = 0; i < SIZE;)
+	//{
+	//	bool flag = true;
+	//	int number = rand() % 10 + 1;
+	//	for (int j = 0; j < i; j++)
+	//	{
+	//		if (number == arr[j])
+	//		{
+	//			flag = false;
+	//			break;
+	//		}
+	//	}
+	//	if (flag)
+	//	{
+	//		arr[i] = number;
+	//		i++;
+	//	}
+	//}
+	//for (size_t i = 0; i < SIZE; i++)
+	//{
+	//	cout << arr[i] << "\t";
+	//}
+	//cout << endl;
+	//for (size_t i = 5; i < SIZE; i--)
+	//{
+	//	cout << arr[i] << "\t";
+	//}
+
+	//task_3
+	//int arr[4];
+	//int number, per=0;
+	//for (int i = 0; i < 5; i++)
+	//{
+	//	cout << "Enter your num :: ";
+	//	cin >> number;
+	//	arr[i] = number;
+	//}
+	//for (size_t i = 0; i < 5; i++)
+	//{
+	//	per += arr[i];
+	//}
+	//cout << per;
+
+	//task_4
+
+	//int arr[12];
+	//int number, sum=0;
+	//int min = arr[0];
+	//int max = arr[0];
+	//int pos_max = 0, pos_min = 0;
+	//for (int i = 0; i < 12; i++)
+	//{
+	//	cout << "Enter your profit for "<<i+1<<" mounth ::";
+	//	cin >> number;
+	//	arr[i] = number;
+	//}
+	//for (int i = 0; i < 12; i++)
+	//{
+	//	if (min > arr[i]) {
+	//		min = arr[i];
+	//		pos_min = i;
+	//	}
+	//	if (max < arr[i]) {
+	//		max = arr[i];
+	//		pos_max = i;
+	//	}
+	//}
+	//int num_max = pos_max + 1;
+	//int num_min = pos_min + 1;
+	///*cout << "Rezault :: " << <<" max "<<<<" min";*/
+	//switch (num_max) {
+	//case 1:
+	//	cout << "max January";
+	//	break;
+	//case 2:
+	//	cout << "max February";
+	//	break;
+	//case 3:
+	//	cout << "max March";
+	//	break;
+	//case 4:
+	//	cout << "max April";
+	//	break;
+	//case 5:
+	//	cout << "max May";
+	//	break;
+	//case 6:
+	//	cout << "max June";
+	//	break;
+	//case 7:
+	//	cout << "max July";
+	//	break;
+	//case 8:
+	//	cout << "max August";
+	//	break;
+	//case 9:
+	//	cout << "max September";
+	//	break;
+	//case 10:
+	//	cout << "max October";
+	//	break;
+	//case 11:
+	//	cout << "max November";
+	//	break;
+	//case 12:
+	//	cout << "max December";
+	//	break;
+	//default:
+	//	cout << "Invalid month";
+	//	break;
+	//}
+
+	//switch (num_min) {
+	//case 1:
+	//	cout << "min January";
+	//	break;
+	//case 2:
+	//	cout << "min February";
+	//	break;
+	//case 3:
+	//	cout << "min March";
+	//	break;
+	//case 4:
+	//	cout << "min April";
+	//	break;
+	//case 5:
+	//	cout << "min May";
+	//	break;
+	//case 6:
+	//	cout << "min June";
+	//	break;
+	//case 7:
+	//	cout << "min July";
+	//	break;
+	//case 8:
+	//	cout << "min August";
+	//	break;
+	//case 9:
+	//	cout << "min September";
+	//	break;
+	//case 10:
+	//	cout << "min October";
+	//	break;
+	//case 11:
+	//	cout << "min November";
+	//	break;
+	//case 12:
+	//	cout << "min December";
+	//	break;
+	//default:
+	//	cout << "Invalid month";
+	//	break;
+	//}
+}
